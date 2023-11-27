c375e7edac42f5182b0ca1cadaa8f72406978c9f
Vishwajeet Panda <panda18vishu@gmail.com>
2023-11-27 15:54:10 +0530

    commiting every files

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        subject = request.form.get("subject")
+        message = request.form.get("message")
+
+        with open("contacts.txt", "a+") as file:
+            file.write(f"{name}, {email}, {subject}, {message}\n")
+
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
